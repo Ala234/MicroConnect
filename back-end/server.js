98d7020ce38ea7ba71c7d2a48ed5bfdf94557a59
Ala234 <s202279560@kfupm.edu.sa>
2026-05-03 00:51:33 +0300

    Configure CORS and add frontend env files

--- back-end/server.js
+++ back-end/server.js
@@ -5,7 +5,13 @@ const connectDB = require('./config/db');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:5174',
+  ],
+  credentials: true,
+}));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
