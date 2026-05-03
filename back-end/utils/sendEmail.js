c8f31ae1285e5d26d75fe4c1028a605b011414b8
Ala234 <s202279560@kfupm.edu.sa>
2026-05-03 10:47:25 +0300

    Force IPv4 globally with DNS

--- back-end/utils/sendEmail.js
+++ back-end/utils/sendEmail.js
@@ -1,10 +1,15 @@
 const nodemailer = require('nodemailer');
+const dns = require('dns');
+
+// Force IPv4 globally for the entire Node process
+dns.setDefaultResultOrder('ipv4first');
 
 const sendEmail = async ({ to, subject, html }) => {
   const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
     port: 587,
     secure: false,
+    requireTLS: true,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
@@ -12,7 +17,6 @@ const sendEmail = async ({ to, subject, html }) => {
     tls: {
       rejectUnauthorized: false,
     },
-    family: 4,
   });
 
   const mailOptions = {
